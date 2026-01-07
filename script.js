a9066e612c7d4be2426403a7a8996d65834c050a
sasindumallawarachchi28@gmail.com <your.email@example.com>
2026-01-07 22:26:25 +0530

    adding form submit

---
+++ script.js
@@ -0,0 +1,14 @@
+function sendEmail() {
+    let params = {
+        fullName: document.getElementById("fullName").value,
+        email: document.getElementById("email").value,
+        phone: document.getElementById("phone").value,
+        serviceType: document.getElementById("serviceType").value,
+        loanAmount: document.getElementById("loanAmount").value,
+        employmentStatus: document.getElementById("employmentStatus").value,
+        monthlyIncome: document.getElementById("monthlyIncome").value,
+        message: document.getElementById("message").value,
+    }
+    emailjs.send("service_qv3xbkk","template_7hsszwn",params)
+}
+
